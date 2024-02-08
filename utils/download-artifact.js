ee5477217a8454d34f7b02c6e38993fbce0c89f1
アレクサンダー.eth <4975670+pavlovcik@users.noreply.github.com>
2024-02-08 20:37:07 +0900

    refactor: clean up code for readability  (#94)
    * refactor: clean up code for readability and to accomodate updated deploy comment schema
    
    * fix: commit sorting

--- utils/download-artifact.js
+++ utils/download-artifact.js
@@ -1,18 +1,16 @@
-module.exports = async ({ github, context, fs, workflow_run_id, workspace, artifact_name }) => {
+module.exports = async function downloadArtifact({ github, context, fs, workflowRunId, workspace, artifactName }) {
   console.log("download_artifact.....");
   const artifacts = await github.rest.actions.listWorkflowRunArtifacts({
     owner: context.repo.owner,
     repo: context.repo.repo,
-    run_id: workflow_run_id,
+    run_id: workflowRunId,
   });
-  const matchArtifact = artifacts.data.artifacts.filter((artifact) => {
-    return artifact.name == artifact_name;
-  })[0];
+  const matchArtifact = artifacts.data.artifacts.filter((artifact) => artifact.name == artifactName)[0];
   const download = await github.rest.actions.downloadArtifact({
     owner: context.repo.owner,
     repo: context.repo.repo,
     artifact_id: matchArtifact.id,
     archive_format: "zip",
   });
-  fs.writeFileSync(`${workspace}/pr.zip`, Buffer.from(download.data));
+  fs.writeFileSync(`${workspace}/pull-request-artifact.zip`, Buffer.from(download.data));
 };
