f83ff234ad023dbeba442e139c49b3fb47824585
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-06-04 01:13:50 +0100

    refactor: user tx history

--- airdrop-cli/src/scripts/user-tx-parser.ts
+++ airdrop-cli/src/scripts/user-tx-parser.ts
@@ -1,24 +1,15 @@
 import { BigNumber, ethers } from "ethers";
 import { SupabaseClient, createClient } from "@supabase/supabase-js";
 import { permit2Abi } from "../abis/permit2Abi";
-import { writeFile } from "fs/promises";
-import { formatUnits } from "viem";
 import { Decoded, ScanResponse, User } from "../types";
 import { SUPABASE_ANON_KEY, SUPABASE_URL, UBQ_OWNERS } from "../utils/constants";
-
+import { getSupabaseData } from "./utils";
 /**
  * Collects permits using Etherscan and Gnosisscan APIs.
  * Does so using the tx history from three sources:
  * 1. Permit2 address: From === Hunter, To === Permit2
  * 2. UBQ wallet addresses: From === UBQ wallet, To === Hunter
  * 3. User wallet addresses: From === Hunter, To === Permit2
- *
- * All sources are combined, duplicates are removed
- * and the final result is written to a file.
- *
- * Outputs:
- * - blockscan-user-permits.json: A list of permits by wallet address.
- * - blockscan-leaderboard.json: A leaderboard of earnings by wallet address.
  *
  * Middle ground of the three methods.
  */
@@ -31,7 +22,6 @@ export class UserBlockTxParser {
   gnosisProvider: ethers.providers.WebSocketProvider;
   userWallets: (string | undefined)[] = [];
   users: User[] = [];
-  userPermits: Record<string, Decoded[]> = {};
   userSigPermits: Record<string, Decoded> = {};
 
   constructor(gnosisApiKey = "WR9YP2CY3NG2WRX8FN5DCNKKIAGIIN83YN", etherscanApiKey = "JPHWVVUBAIP1UVQZSSDKV73YX48I2M7SWV") {
@@ -54,57 +44,14 @@ export class UserBlockTxParser {
 
   async run() {
     const loader = this.loader();
+    const { idToWalletMap, users } = await getSupabaseData(this.sb);
+    this.users = users;
 
-    // collect user info
-    const { idToWalletMap } = await this.getSupabaseData();
-
-    // collect wallet addresses
     const userWalletIds = this.users.map((user) => user.wallet_id);
     this.userWallets = userWalletIds.map((id) => idToWalletMap.get(id)?.toLowerCase());
 
     await this.batcher();
-
-    await writeFile("src/scripts/data/user-tx-permits.json", JSON.stringify(this.userPermits, null, 2));
-    await writeFile("src/scripts/data/user-sig-permits.json", JSON.stringify(this.userSigPermits, null, 2));
-
-    // process all permits
-    await this.leaderboard(this.userPermits);
     clearInterval(loader);
-
-    console.log(`[UserBlockTxParser] Finished processing ${Object.keys(this.userPermits).length} users.`);
-  }
-
-  async leaderboard(data: Record<string, Decoded[]>) {
-    const leaderboard: Record<string, number> = {};
-    const userPermits = data;
-    const users = Object.keys(userPermits);
-
-    // calculate score for each user
-    for (const user of users) {
-      const permits = userPermits[user as keyof typeof userPermits];
-      let score = 0;
-      for (const permit of permits) {
-        const amount = permit.permitted.amount;
-
-        score += parseFloat(formatUnits(BigInt(amount), 18));
-      }
-
-      leaderboard[user] = score;
-    }
-
-    // sort leaderboard by score
-    // reduce to object
-    // write to file
-    await writeFile(
-      "src/scripts/data/user-tx-leaderboard.json",
-      JSON.stringify(
-        Object.entries(leaderboard)
-          .sort((a, b) => b[1] - a[1])
-          .reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {}),
-        null,
-        2
-      )
-    );
   }
 
   async batcher() {
@@ -127,14 +74,14 @@ export class UserBlockTxParser {
     let etxs: ScanResponse[] = [];
 
     if (!Array.isArray(address)) {
-      gtxs = await this.getGnosisTxs(address);
-      etxs = await this.getEthTxs(address);
+      gtxs = await this.getChainTx(address, undefined, undefined, true, 100);
+      etxs = await this.getChainTx(address, undefined, undefined, true, 1);
     } else {
       for (const addr of address) {
         if (!addr) continue;
 
-        const _gtxs = await this.getGnosisTxs(addr);
-        const _etxs = await this.getEthTxs(addr);
+        const _gtxs = await this.getChainTx(addr, undefined, undefined, true, 100);
+        const _etxs = await this.getChainTx(addr, undefined, undefined, true, 1);
 
         gtxs.push(..._gtxs);
         etxs.push(..._etxs);
@@ -151,12 +98,7 @@ export class UserBlockTxParser {
       const decoded = this.decodePermit(log);
       const sig = decoded.signature.toLowerCase();
 
-      if (!this.userPermits[indexerAddress]) {
-        this.userPermits[indexerAddress] = [];
-      }
-
       this.userSigPermits[sig] = decoded;
-      this.userPermits[indexerAddress].push(decoded);
     }
   }
 
@@ -167,69 +109,22 @@ export class UserBlockTxParser {
     return { eth, gnosis };
   }
 
-  async getEthTxs(address: string, from?: number, to?: number, filter = true): Promise<ScanResponse[]> {
-    const toBlock = to ?? (await this.getBlockNumbers()).eth;
-    const fromBlock = from ?? 10373290; // ~3yrs ago 29/05/2024
-    const url = `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=${fromBlock}&endblock=${toBlock}&page=1&offset=1000&sort=asc&apikey=${this.etherscanApiKey}`;
+  async getChainTx(address: string, from?: number, to?: number, filter = true, chainId = 100): Promise<ScanResponse[]> {
+    const chain = chainId === 1 ? "eth" : "gnosis";
+    const toBlock = to ?? (await this.getBlockNumbers())[chain];
+    const fromBlock = chain === "eth" ? 10373290 : 15349006; // ~3yrs ago 29/05/2024
+    const url = `https://api.${chain}.io/api?module=account&action=txlist&address=${address}&startblock=${fromBlock}&endblock=${toBlock}&page=1&offset=1000&sort=asc&apikey=${this.etherscanApiKey}`;
     const response = await (await fetch(url)).json();
     const methodId = "0x30f28b7a";
 
     if (response.result === "Max rate limit reached") {
       console.log("Rate limit reached, waiting 3s before retrying...");
       await new Promise((resolve) => setTimeout(resolve, 3000));
-      return this.getEthTxs(address, from, to, filter);
+      return this.getChainTx(address, from, to, filter, chainId);
     }
 
     if (!filter) return response.result as ScanResponse[];
     return response.result.filter((tx: ScanResponse) => tx.input.startsWith(methodId)) as ScanResponse[];
-  }
-
-  async getGnosisTxs(address: string, from?: number, to?: number, filter = true): Promise<ScanResponse[]> {
-    const toBlock = to ?? (await this.getBlockNumbers()).gnosis;
-    const fromBlock = from ?? 15349006; // ~3yrs ago 29/05/2024
-
-    const url = `https://api.gnosisscan.io/api?module=account&action=txlist&address=${address}&startblock=${fromBlock}&endblock=${toBlock}&page=1&offset=1000&sort=asc&apikey=${this.gnosisApiKey}`;
-    const response = await (await fetch(url)).json();
-    const methodId = "0x30f28b7a";
-
-    if (response.result === "Max rate limit reached") {
-      console.log("Rate limit reached, waiting 3s before retrying...");
-      await new Promise((resolve) => setTimeout(resolve, 3000));
-      return this.getGnosisTxs(address, from, to, filter);
-    }
-
-    if (!filter) return response.result as ScanResponse[];
-    return response.result.filter((tx: ScanResponse) => tx.input.startsWith(methodId)) as ScanResponse[];
-  }
-
-  async getSupabaseData(): Promise<{ walletToIdMap: Map<string, number>; idToWalletMap: Map<number, string>; users: User[] }> {
-    const walletToIdMap = new Map<string, number>();
-    const idToWalletMap = new Map<number, string>();
-
-    const { data, error } = await this.sb.from("wallets").select("address, id");
-
-    if (error || !data?.length) {
-      console.error(error);
-
-      return { walletToIdMap, idToWalletMap, users: [] };
-    }
-
-    for (const wallet of data) {
-      const addr = wallet.address.toLowerCase();
-      walletToIdMap.set(addr, wallet.id);
-      idToWalletMap.set(wallet.id, addr);
-    }
-
-    const { data: users, error: rr } = await this.sb.from("users").select("*").in("wallet_id", Array.from(idToWalletMap.keys()));
-
-    if (rr || !users?.length) {
-      console.error(rr);
-      return { walletToIdMap, idToWalletMap, users: [] };
-    }
-
-    this.users = users;
-
-    return { walletToIdMap, idToWalletMap, users };
   }
 
   decodePermit(data: ScanResponse): Decoded {
