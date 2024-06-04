a2f29c6d38e9b4901670615203044fc3ec624d6c
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-06-04 01:12:42 +0100

    refactor: controller code split

--- airdrop-cli/src/scripts/data-controller.ts
+++ airdrop-cli/src/scripts/data-controller.ts
@@ -4,10 +4,10 @@ import { DuneDataParser } from "./dune-data-parser";
 // import { PopulateDB } from "./populate-db";
 import { Decoded, FinalData, PermitDetails, ScanResponse, User } from "../types";
 import { writeFile } from "fs/promises";
-import { PERMIT2_ADDRESS, Tokens, UBQ_OWNERS } from "../utils/constants";
-import { BigNumber, BigNumberish, ethers } from "ethers";
-import { permit2Abi } from "../abis/permit2Abi";
+import { Tokens, UBQ_OWNERS } from "../utils/constants";
+import { ethers } from "ethers";
 import { formatUnits } from "viem";
+import { txFinder } from "./tx-finding";
 
 import ISSUE_USER_WALLET_PERMITS from "./data/paid-out-wallet-permits.json";
 import ISSUE_USER_SIG_PERMITS from "./data/paid-out-sig-permits.json";
@@ -34,7 +34,10 @@ import DUNE_SIG_PERMITS from "./data/dune-sig-map.json";
  * 2. Match on-chain data with off-chain data
  * 3. Prepare the data for the database
  * 4. Populate the database
- *
+ * 
+singles length:  415 w/o onchain data + 21 invalidated nonces
+doubles length:  270
+triples length:  239
  */
 
 export class DataController {
@@ -60,6 +63,7 @@ export class DataController {
   triples: Record<string, FinalData> = {};
 
   nonceMap: Map<string, FinalData[]> = new Map();
+  withoutIssueNumberOrRepoName: Record<string, FinalData> = {};
 
   constructor() {
     this.issueParser = new PaidIssueParser();
@@ -70,7 +74,7 @@ export class DataController {
 
   async run() {
     await this.gatherData();
-    await this.matchThree();
+    await this.matchAll();
     await this.rescanOnchainForSingles();
     await this.findAndRemoveInvalidatedNonces();
     await this.filterSets();
@@ -78,18 +82,17 @@ export class DataController {
   }
 
   async findAndRemoveInvalidatedNonces() {
-    const invalidatedNonces = [] as { owner: string; nonce: string; wordPos: string; bitPos: string }[];
+    const invalidatedNonces = [] as { hash: string; owner: string; nonce: string; wordPos: string; bitPos: string }[];
     for await (const owner of UBQ_OWNERS) {
       const scans: ScanResponse[][] = [];
 
-      scans.push(await this.userTxParser.getGnosisTxs(owner, undefined, undefined, false));
-      scans.push(await this.userTxParser.getEthTxs(owner, undefined, undefined, false));
+      scans.push(
+        await this.userTxParser.getChainTx(owner, undefined, undefined, false, 1),
+        await this.userTxParser.getChainTx(owner, undefined, undefined, false, 100)
+      );
 
       const filteredScans = scans.flat().filter((scan) => scan.methodId === "0x3ff9dcb1");
       if (filteredScans.length === 0) continue;
-
-      console.log(`Found ${filteredScans.length} invalidated nonces for ${owner}`);
-      await writeFile("src/scripts/data/dc-invalidated-nonces.json", JSON.stringify(filteredScans, null, 2));
 
       for (const scan of filteredScans) {
         const invalidated = await this.decodeInvalidate(scan);
@@ -99,6 +102,7 @@ export class DataController {
             wordPos: invalidated.wordPos.toString(),
             bitPos: invalidated.bitPos.toString(),
             owner: owner,
+            hash: scan.hash,
           });
         }
       }
@@ -116,6 +120,11 @@ export class DataController {
       if (permit.blockTimestamp && permit.commentTimestamp) {
         delete this.singles[sig];
         this.doubles[sig] = permit;
+      }
+
+      if (!permit.repoName || !permit.issueNumber) {
+        delete this.singles[sig];
+        this.withoutIssueNumberOrRepoName[sig] = permit;
       }
     }
 
@@ -164,149 +173,49 @@ export class DataController {
 
         if (gnoOnlyWithValue.length === 0 && ethOnlyWithValue.length === 0) continue;
 
-        await this.txFinder(gnoOnlyWithValue, user);
-        await this.txFinder(ethOnlyWithValue, user);
+        await txFinder(gnoOnlyWithValue, user, this.userTxParser);
+        await txFinder(ethOnlyWithValue, user, this.userTxParser);
       }
     }
   }
 
-  async txFinder(userSingles: FinalData[], user: string) {
-    if (!userSingles || userSingles.length === 0) return;
-    const networkID = userSingles[0].token.toLowerCase() === Tokens.WXDAI ? "100" : "1";
-    const scans: ScanResponse[][] = [];
+  mergedFinalAndDecoded(single: FinalData, found: Decoded) {
+    const signature = found.signature.toLowerCase();
+    const userTxMapHasSig = this.userTxSigMap[signature];
+    const duneMapHasSig = this.duneSigMap[signature];
 
-    if (networkID === "100") {
-      scans.push(await this.userTxParser.getGnosisTxs(user, undefined, undefined, false));
-    } else if (networkID === "1") {
-      scans.push(await this.userTxParser.getEthTxs(user, undefined, undefined, false));
-    }
+    const merged: FinalData & Decoded = {
+      // DC
+      nonce: single.nonce ?? found.nonce,
+      permitted: found.permitted,
+      signature: found.signature ?? single.signature,
+      to: single.to ?? found.to,
+      txHash: found.txHash ?? single.txHash,
+      blockTimestamp: found.blockTimestamp ?? single.blockTimestamp,
+      issueNumber: single.issueNumber ?? found.issueNumber,
+      permitOwner: single.owner ?? found.permitOwner,
+      repoName: single.repoName ?? found.repoName,
+      // FD
+      amount: single.amount ?? parseFloat(formatUnits(BigInt(found.permitted.amount), 18)),
+      owner: single.owner ?? found.permitOwner,
+      token: single.token ?? found.permitted.token,
+      commentTimestamp: single.commentTimestamp,
+    };
 
-    const filteredScans = scans.flat().filter((scan) => scan.methodId === "0x30f28b7a");
-    if (filteredScans.length === 0) return null;
-
-    const decodedPermits = filteredScans.map((scan) => this.userTxParser.decodePermit(scan));
-    if (decodedPermits.length === 0) return null;
-
-    return await this.findTx(userSingles, decodedPermits);
-  }
-
-  async findTx(userSingles: FinalData[], decodedPermits: Decoded[]) {
-    for (const single of userSingles) {
-      if (single.commentTimestamp && !single.blockTimestamp) {
-        // we know when the permit was generated
-        const isInvalid = await this.isNonceValid(single.nonce, single.owner, single.token);
-        if (isInvalid) {
-          /**
-           * Now this could mean either that the nonce was
-           * invalidated by the owner or the permit has been claimed
-           */
-          single.claimed = true;
-          continue;
-        }
-
-        const found = await this.findNearestTx(decodedPermits, single);
-
-        if (!found) {
-          single.blockTimestamp = null;
-          single.claimed = false;
-          continue;
-        }
-
-        single.blockTimestamp = found.blockTimestamp;
-        single.claimed = true;
-        single.txHash = found.txHash;
-
-        await this.matchThree([single, found]);
-      } else if (!(single.issueNumber && single.repoName && single.commentTimestamp) && single.blockTimestamp && !single.commentTimestamp) {
-        // we know when the permit was used
-      }
-    }
-  }
-
-  async findNearestTx(decodedPermits: Decoded[], permit: FinalData) {
-    const decodedTimestamp = new Date(permit.commentTimestamp ?? "0").getTime();
-
-    const matchingDecodedPermits = decodedPermits.filter((decoded) => {
-      const pAmount = parseFloat(permit.amount.toString());
-      const dAmount = parseFloat(formatUnits(BigInt(decoded.permitted.amount), 18));
-      return pAmount === dAmount;
-    });
-
-    if (!matchingDecodedPermits.length) return null;
-
-    return matchingDecodedPermits.reduce((acc, match) => {
-      const matchDate = new Date(match.blockTimestamp ?? "0").getTime();
-      const accDate = new Date(acc.blockTimestamp ?? "0").getTime();
-      const decodedDate = new Date(decodedTimestamp).getTime();
-      const isAmountMatch = parseFloat(permit.amount.toString()) === parseFloat(formatUnits(BigInt(match.permitted.amount), 18));
-
-      if (!isAmountMatch) return acc;
-
-      if (matchDate < decodedDate) {
-        return match;
-      }
-
-      if (matchDate > accDate) {
-        return match;
-      }
-
-      return acc;
-    });
-  }
-
-  async matchThree(data?: [FinalData, Decoded]) {
-    if (data) {
-      const [single, found] = data;
-      const signature = found.signature.toLowerCase();
-
-      const userTxMapHasSig = this.userTxSigMap[signature];
-      const duneMapHasSig = this.duneSigMap[signature];
-
-      const merged: FinalData & Decoded = {
-        // DC
-        nonce: single.nonce ?? found.nonce,
-        permitted: found.permitted,
-        signature: found.signature ?? single.signature,
-        to: single.to ?? found.to,
-        txHash: found.txHash ?? single.txHash,
-        blockTimestamp: found.blockTimestamp ?? single.blockTimestamp,
-        issueNumber: single.issueNumber ?? found.issueNumber,
-        permitOwner: single.owner ?? found.permitOwner,
-        repoName: single.repoName ?? found.repoName,
-        // FD
-        amount: single.amount ?? parseFloat(formatUnits(BigInt(found.permitted.amount), 18)),
-        owner: single.owner ?? found.permitOwner,
-        token: single.token ?? found.permitted.token,
-        commentTimestamp: single.commentTimestamp,
-      };
-
-      if (merged.blockTimestamp && merged.commentTimestamp) {
-        merged.claimed = true;
-      }
-
-      if (userTxMapHasSig && duneMapHasSig) {
-        this.triples[signature] = merged;
-      } else if (userTxMapHasSig || duneMapHasSig) {
-        this.doubles[signature] = merged;
-      } else {
-        this.singles[signature] = merged;
-      }
-
-      this.finalData[merged.to.toLowerCase()].push(merged);
-      this.finalDataViaSig[signature] = merged;
-
-      const nonceMap = this.nonceMap.get(merged.nonce);
-      if (nonceMap) {
-        this.nonceMap.set(merged.nonce, [...nonceMap, merged]);
-      } else {
-        this.nonceMap.set(merged.nonce, [merged]);
-      }
-
+    this.finalDataViaSig[signature] = merged;
+    if (userTxMapHasSig && duneMapHasSig) {
+      this.triples[signature] = merged;
+      return;
+    } else if (userTxMapHasSig || duneMapHasSig) {
+      this.doubles[signature] = merged;
       return;
     }
 
-    const allSigs = [...Object.keys(this.userTxSigMap), ...Object.keys(this.duneSigMap), ...Object.keys(this.issueSigMap)];
+    this.singles[signature] = merged;
+  }
 
+  async matchAll() {
+    const allSigs = [...Object.keys(this.userTxSigMap), ...Object.keys(this.duneSigMap), ...Object.keys(this.issueSigMap)];
     allSigs.forEach((sig) => {
       const userTxPermit = this.userTxSigMap[sig as keyof typeof this.userTxSigMap] as unknown as Decoded;
       const dunePermit = this.duneSigMap[sig as keyof typeof this.duneSigMap] as unknown as Decoded;
@@ -421,34 +330,6 @@ export class DataController {
     return { nonce, wordPos, bitPos };
   }
 
-  async isNonceValid(nonce: string, owner: string, token: string) {
-    const ethProvider = this.userTxParser.ethProvider;
-    const gnosisProvider = this.userTxParser.gnosisProvider;
-    const activeProvider = token === Tokens.WXDAI ? gnosisProvider : ethProvider;
-
-    return await this.invalidateNonce(nonce, owner, activeProvider);
-  }
-
-  nonceBitmap(nonce: BigNumberish): { wordPos: BigNumberish; bitPos: number } {
-    // wordPos is the first 248 bits of the nonce
-    const wordPos = BigNumber.from(nonce).shr(8);
-    // bitPos is the last 8 bits of the nonce
-    const bitPos = BigNumber.from(nonce).and(255).toNumber();
-    return { wordPos, bitPos };
-  }
-
-  async invalidateNonce(nonce: string, owner: string, provider: ethers.providers.WebSocketProvider): Promise<boolean> {
-    const permit2Contract = new ethers.Contract(PERMIT2_ADDRESS, permit2Abi, provider);
-
-    const { wordPos, bitPos } = this.nonceBitmap(BigNumber.from(nonce));
-    const bitmap = await permit2Contract.nonceBitmap(owner, wordPos);
-
-    const bit = BigNumber.from(1).shl(bitPos);
-    const flipped = BigNumber.from(bitmap).xor(bit);
-
-    return bit.and(flipped).eq(0);
-  }
-
   async leaderboard() {
     const leaderboard: Record<string, number> = {};
     const claimedLeaderboard: Record<string, number> = {};
@@ -457,7 +338,7 @@ export class DataController {
 
     for (const [user, permits] of Object.entries(this.finalData)) {
       for (const permit of permits) {
-        const sig = permit.signature?.toLowerCase();
+        const sig = permit.nonce;
         if (!sig) continue;
         if (deduped.has(sig)) continue;
 
@@ -474,9 +355,11 @@ export class DataController {
       }
     }
 
-    this.finalData = newFinal;
-
-    await writeFile("src/scripts/data/dc-final-data.json", JSON.stringify(this.finalData, null, 2));
+    await writeFile("src/scripts/data/dc-singles.json", JSON.stringify(this.singles, null, 2));
+    await writeFile("src/scripts/data/dc-doubles.json", JSON.stringify(this.doubles, null, 2));
+    await writeFile("src/scripts/data/dc-triples.json", JSON.stringify(this.triples, null, 2));
+    await writeFile("src/scripts/data/dc-without-issue-or-repo.json", JSON.stringify(this.withoutIssueNumberOrRepoName, null, 2));
+    await writeFile("src/scripts/data/dc-final-data.json", JSON.stringify(newFinal, null, 2));
     await writeFile(
       "src/scripts/data/dc-leaderboard.json",
       JSON.stringify(
@@ -506,19 +389,17 @@ export class DataController {
     this.users = userInfo.users;
     this.walletToIdMap = userInfo.walletToIdMap;
 
-    const userTxWalletPermits = USER_TX_WALLET_PERMITS;
-    const duneWalletPermits = DUNE_PERMITS;
-    const issueWalletPermits = ISSUE_USER_WALLET_PERMITS;
+    await this.issueParser.run();
+    await this.userTxParser.run();
+    await this.duneParser.run();
 
-    this.issueSigMap = ISSUE_USER_SIG_PERMITS as unknown as Record<string, IssueOut>;
-    this.duneSigMap = DUNE_SIG_PERMITS as unknown as Record<string, Decoded>;
-    this.userTxSigMap = USER_TX_SIG_PERMITS as unknown as Record<string, Decoded>;
+    this.issueSigMap = this.issueParser.sigPaymentInfo;
+    this.duneSigMap = this.duneParser.duneSigMap;
+    this.userTxSigMap = this.userTxParser.userTxSigMap;
 
-    return {
-      userTxHistoryPermits: { userTxWalletPermits },
-      duneData: { duneWalletPermits },
-      issueData: { issueWalletPermits },
-    };
+    // this.issueSigMap = ISSUE_USER_SIG_PERMITS as unknown as Record<string, IssueOut>;
+    // this.duneSigMap = DUNE_SIG_PERMITS as unknown as Record<string, Decoded>;
+    // this.userTxSigMap = USER_TX_SIG_PERMITS as unknown as Record<string, Decoded>;
   }
 }
 
